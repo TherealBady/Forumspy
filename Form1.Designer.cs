c6954aad05a1ec6a803403517ba8afc4ad3f1f95
TherealBady <aliev000@gmail.com>
2015-12-15 19:05:21 +0100

    Forumspy source code
    Muffin

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Forumpointspy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Forumpointspy));
+            this.linkbar = new System.Windows.Forms.TextBox();
+            this.stratstop = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.checkinterval = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.savelog = new System.Windows.Forms.Button();
+            this.checktimer = new System.Windows.Forms.Timer(this.components);
+            this.logtext = new System.Windows.Forms.RichTextBox();
+            this.topicreaders = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.checkinterval)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // linkbar
+            // 
+            this.linkbar.Location = new System.Drawing.Point(12, 25);
+            this.linkbar.Name = "linkbar";
+            this.linkbar.Size = new System.Drawing.Size(424, 20);
+            this.linkbar.TabIndex = 1;
+            // 
+            // stratstop
+            // 
+            this.stratstop.Location = new System.Drawing.Point(489, 13);
+            this.stratstop.Name = "stratstop";
+            this.stratstop.Size = new System.Drawing.Size(112, 23);
+            this.stratstop.TabIndex = 2;
+            this.stratstop.Text = "Start spying!";
+            this.stratstop.UseVisualStyleBackColor = true;
+            this.stratstop.Click += new System.EventHandler(this.stratstop_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(388, 349);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(117, 23);
+            this.clear.TabIndex = 3;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // checkinterval
+            // 
+            this.checkinterval.Location = new System.Drawing.Point(12, 91);
+            this.checkinterval.Maximum = new decimal(new int[] {
+            86400,
+            0,
+            0,
+            0});
+            this.checkinterval.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.checkinterval.Name = "checkinterval";
+            this.checkinterval.Size = new System.Drawing.Size(120, 20);
+            this.checkinterval.TabIndex = 5;
+            this.checkinterval.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.checkinterval.ValueChanged += new System.EventHandler(this.checkinterval_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Check interval(sec):";
+            // 
+            // savelog
+            // 
+            this.savelog.Location = new System.Drawing.Point(511, 349);
+            this.savelog.Name = "savelog";
+            this.savelog.Size = new System.Drawing.Size(117, 23);
+            this.savelog.TabIndex = 7;
+            this.savelog.Text = "Save...";
+            this.savelog.UseVisualStyleBackColor = true;
+            this.savelog.Click += new System.EventHandler(this.savelog_Click);
+            // 
+            // checktimer
+            // 
+            this.checktimer.Interval = 5000;
+            this.checktimer.Tick += new System.EventHandler(this.checktimer_Tick);
+            // 
+            // logtext
+            // 
+            this.logtext.Location = new System.Drawing.Point(12, 117);
+            this.logtext.Name = "logtext";
+            this.logtext.Size = new System.Drawing.Size(616, 214);
+            this.logtext.TabIndex = 8;
+            this.logtext.Text = "";
+            // 
+            // topicreaders
+            // 
+            this.topicreaders.Location = new System.Drawing.Point(213, 51);
+            this.topicreaders.Name = "topicreaders";
+            this.topicreaders.Size = new System.Drawing.Size(415, 65);
+            this.topicreaders.TabIndex = 9;
+            this.topicreaders.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(133, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Forumreaders:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label3.Location = new System.Drawing.Point(15, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(325, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Forum link(It works only Ip board forums and just inside of the topic):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 349);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(169, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "This program is under zlib licence. ";
+            // 
+            // Forumpointspy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 384);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.topicreaders);
+            this.Controls.Add(this.logtext);
+            this.Controls.Add(this.savelog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkinterval);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.stratstop);
+            this.Controls.Add(this.linkbar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Forumpointspy";
+            this.Text = "Forumspy";
+            ((System.ComponentModel.ISupportInitialize)(this.checkinterval)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox linkbar;
+        private System.Windows.Forms.Button stratstop;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.NumericUpDown checkinterval;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button savelog;
+        private System.Windows.Forms.Timer checktimer;
+        private System.Windows.Forms.RichTextBox logtext;
+        private System.Windows.Forms.RichTextBox topicreaders;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Net;
+using System.Globalization;
+
+public struct ponted
+{
+   public string poster;
+   public int postid;
+    public Int32 point;
+}        
+ 
+
+namespace WindowsFormsApplication1
+{
+
+   
+    public partial class Forumpointspy : Form
+    {
+        List<string> momentreader = new List<string>();
+        List<string> refmomentreader = new List<string>();
+       
+
+      
+        List<ponted> posterlist = new List<ponted>();
+
+        string posternamestart = "<span class=\"author vcard\"><a class=\"url fn\" href='http://forum.hunbrony.hu/index.php?/user";
+        string postedbetweenstart = "<span class=\"author vcard\"><a class=\"url fn\" href='";
+        string postedbetweenend = "'>";
+        //string postedbeveenstart = "/'>";
+        //string posternameend = "</a>&nbsp;<a href='http://forum.hunbrony.hu/index.php?/user/";
+       // string postedtime = "Elküldve: <abbr class=\"published\" title=\"";
+        string getpoint = "ipb.global.registerReputation( 'rep_post_";
+        string idafter = "', { app: 'forums', type: 'pid', typeid: '";
+        string pointbetweenstart = "' }, parseInt('";
+        string pointbetweenend = "') );";
+
+
+        string readerliststart = "<div id='topic_active_users' class='active_users'>";
+        string readerlistend = "<!-- Close topic -->";
+        string readerstart = "<li><a href='";
+        string readerend = "' title='";
+
+        public Forumpointspy()
+        {
+            InitializeComponent();
+            checkinterval.Value = checktimer.Interval/1000;
+
+        }
+
+        private void stratstop_Click(object sender, EventArgs e)
+        {
+
+
+
+
+            if (!checktimer.Enabled)
+            {
+                checktimer.Start();
+                linkbar.Enabled = false;
+                checkinterval.Enabled = false;
+                //logtext.ForeColor = Color.Blue;
+                DateTime localDate = DateTime.Now;
+                var culture = new CultureInfo("hu-HU");
+                string date = localDate.ToString(culture);
+                stratstop.Text = "Stop spying!";
+                string appendst = date+" - checking started at - " + linkbar.Text + " - with " + Convert.ToString(checktimer.Interval / 1000) + " second interval ";
+                logtext.SelectionStart = logtext.TextLength;
+                logtext.SelectionLength = appendst.Length;
+                logtext.AppendText(appendst);
+                //logtext.Find(appendst);
+                logtext.SelectionColor = Color.Blue;
+                logtext.AppendText(System.Environment.NewLine);
+                //logtext.Text = "";
+                posterlist.Clear();
+                checknews();
+            }
+            else
+            {
+                //logtext.ForeColor = Color.Green;
+                DateTime localDate = DateTime.Now;
+                var culture = new CultureInfo("hu-HU");
+                string date = localDate.ToString(culture);
+                stratstop.Text = "Start spying!";
+               string appendst =date+" - Checking stopped.";
+               logtext.SelectionStart = logtext.TextLength;
+               logtext.SelectionLength = appendst.Length;
+               logtext.AppendText(appendst);
+               logtext.SelectionColor = Color.Green;
+                logtext.AppendText( System.Environment.NewLine);
+                checkinterval.Enabled = true;
+                checktimer.Stop();
+                linkbar.Enabled = true;
+                momentreader.Clear();
+                refmomentreader.Clear();
+            }
+        }
+
+        private void checknews()
+        {
+            WebClient client = new WebClient();
+            string htmlCode="";
+            try
+            {
+              
+                htmlCode = client.DownloadString(linkbar.Text);
+            }
+            catch
+            {
+                /*
+                string appendst = "Error, something terrible happened, checking stopped.";
+                logtext.SelectionStart = logtext.TextLength;
+                logtext.SelectionLength = appendst.Length;
+                logtext.AppendText(appendst);
+                //logtext.Find(appendst);
+                logtext.SelectionColor = Color.Red;
+                checktimer.Stop();
+                linkbar.Enabled = true;
+                checkinterval.Enabled = true;
+                stratstop.Text = "Start spying!";
+               logtext.AppendText( System.Environment.NewLine);
+                 */
+            }
+
+            checknewpoints(htmlCode);
+
+        }
+
+        private bool checknewpoints(string htmlcode)
+        {
+
+
+           
+            using (StringReader sr = new StringReader(htmlcode))
+            {
+                //int usercounter = -1;
+                string line;
+                string anoneposter = "";
+                bool getactplayers= false;
+                ponted oneposter = new ponted();
+                while ((line = sr.ReadLine()) != null)
+                {
+
+
+                    //if (line.Contains(getpoint)) catchpoint = true;
+                    anoneposter += line;
+                    if (line.Contains(readerliststart))
+                    {
+                        getactplayers = true;
+                        topicreaders.Clear();
+                        refmomentreader.Clear();
+
+                    }
+                    if (line.Contains(readerstart) && getactplayers)
+                    {
+                        //topicreaders.AppendText("megneztem, most mi van?");
+                        string postter = getBetween(line, readerstart, readerend);
+                        
+                        refmomentreader.Add(postter);
+                        //logtext.AppendText("a "+postter);
+                        //logtext.AppendText(System.Environment.NewLine);
+                        topicreaders.AppendText(postter);
+                        topicreaders.AppendText(System.Environment.NewLine);
+
+                    }
+                    //if (getactplayers) 
+                       // logtext.AppendText(line);
+
+                    if (line.Contains(readerlistend))
+                    {
+                        List<string> savecont = new List<string>();
+                        for (int k = 0; k < refmomentreader.Count; k++) savecont.Add(refmomentreader[k]);
+
+                        for (int i = 0; i < refmomentreader.Count; i++)
+                        {
+
+                            for (int j = 0; j < momentreader.Count; j++)
+                            {
+                                if (refmomentreader[i] == momentreader[j])
+                                {
+
+                                    i = 0;
+                                    j = 0;
+                                    refmomentreader.RemoveAt(i);
+                                    momentreader.RemoveAt(j);
+                                  
+                                }
+                            }
+                        }
+
+                        if (refmomentreader.Count > 0)
+                        {
+                            DateTime localDate = DateTime.Now;
+                            var culture = new CultureInfo("hu-HU");
+                            string date = localDate.ToString(culture);
+                            logtext.AppendText(date+" - Új vendég:");
+                            logtext.AppendText(System.Environment.NewLine);
+                            for (int i = 0; i < refmomentreader.Count; i++)
+                            {
+                                logtext.AppendText(refmomentreader[i]);
+                                logtext.AppendText(System.Environment.NewLine);
+                            }
+                            refmomentreader.Clear();
+                        }
+
+                        if (momentreader.Count > 0)
+                        {
+                            DateTime localDate = DateTime.Now;
+                            var culture = new CultureInfo("hu-HU");
+                            string date = localDate.ToString(culture);
+                            logtext.AppendText(date+" - Kilépett a topikból:");
+                            logtext.AppendText(System.Environment.NewLine);
+                            for (int i = 0; i < momentreader.Count; i++)
+                            {
+                                logtext.AppendText(momentreader[i]);
+                                logtext.AppendText(System.Environment.NewLine);
+                            }
+                            momentreader.Clear();
+                        }
+
+                        //momentreader.Clear();
+                        for (int k = 0; k < savecont.Count; k++) momentreader.Add(savecont[k]);
+                           
+
+                        getactplayers = false;
+                        if (momentreader.Count == 0)
+                        {
+                            topicreaders.AppendText("Jelelnleg senki sem olvassa ezt a topikot");
+                            topicreaders.AppendText(System.Environment.NewLine);
+                        }
+                    }
+                    
+                    
+                    if (line.Contains(posternamestart))
+                    {
+                        string poster = getBetween(anoneposter, postedbetweenstart, postedbetweenend);
+                        if (poster != "") oneposter.poster = poster;
+    
+                    }
+
+                    if (line.Contains(getpoint))
+                    {
+                        string getposstid = getBetween(line, getpoint, idafter);
+                        /*
+                        logtext.AppendText(getposstid);
+                        logtext.AppendText(System.Environment.NewLine);
+                        logtext.AppendText(line);
+                        logtext.AppendText(System.Environment.NewLine);
+                        */
+                        oneposter.postid = Convert.ToInt32(getposstid);
+
+                        string chatchpointstring = getBetween(line, pointbetweenstart, pointbetweenend);
+                        oneposter.point = Convert.ToInt32(chatchpointstring);
+
+                        bool foundid = false;
+                        for (int i = 0; i < posterlist.Count; i++)
+                        {
+
+                            if (oneposter.postid == posterlist[i].postid)
+                            {
+                                if (oneposter.point != posterlist[i].point)
+                                {
+                                    //logtext.AppendText(appendst);
+                                    int newpoint = oneposter.point - posterlist[i].point;
+                                    string plusn = "";
+                                    if (newpoint > 0) plusn = "+";
+                                    DateTime localDate = DateTime.Now;
+                                    var culture = new CultureInfo("hu-HU");
+                                    string date = localDate.ToString(culture);
+                                    string appendst =date+" - "+ Convert.ToString(i) + ". hozzászólás(id.:" + posterlist[i].postid + ") pontváltozás: " + plusn + Convert.ToString(newpoint);
+                                    logtext.SelectionStart = logtext.TextLength;
+                                    logtext.SelectionLength = appendst.Length;
+                                    logtext.AppendText(appendst);
+                                    logtext.AppendText(System.Environment.NewLine);
+                                    if (newpoint > 0) logtext.SelectionColor = Color.Green;
+                                    if (newpoint < 0) logtext.SelectionColor = Color.Red;
+                                    if (momentreader.Count > 0)
+                                    {
+                                        logtext.AppendText("lehetséges pontozók:");
+                                        logtext.AppendText(System.Environment.NewLine);
+                                        for (int j = 0; j < momentreader.Count; j++) logtext.AppendText(momentreader[j]);
+
+                                    }
+                                    else logtext.AppendText("Hit and run! :/");
+                                    logtext.AppendText(System.Environment.NewLine);
+
+                                }
+                                posterlist[i] = oneposter;
+                                i = posterlist.Count;
+                                foundid = true;
+                            }
+                        }
+                        if (!foundid)
+                        {
+                            //posterlist.Add(oneposter);
+                            posterlist.Add(oneposter);
+                            DateTime localDate = DateTime.Now;
+                            var culture = new CultureInfo("hu-HU");
+                            string date = localDate.ToString(culture);
+                            logtext.AppendText(date+" - Új poszt hozzáadva! Poszt id.:" + Convert.ToString(oneposter.postid) + " Aktuális pont: " + Convert.ToInt32(oneposter.point));
+                            logtext.AppendText(System.Environment.NewLine);
+                            logtext.AppendText("Poszt tulaj: " + oneposter.poster);
+                            logtext.AppendText(System.Environment.NewLine);
+                        }
+                        //logtext.AppendText(oneposter.poster+" - "+Convert.ToString(oneposter.point));
+                        //logtext.AppendText(System.Environment.NewLine);
+                        anoneposter = "";
+
+
+                    }
+
+                    
+                }//while
+            }//using
+
+
+
+
+            return false;
+        }
+
+
+        public static string getBetween(string strSource, string strStart, string strEnd)
+        {
+
+            int Start, End;
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        private string stringfromstring(string str, int maxLength)
+        {
+            return str.Substring(0, Math.Min(str.Length, maxLength));
+        }
+        private void checktimer_Tick(object sender, EventArgs e)
+        {
+            checknews();
+            //MessageBox.Show("lefutottam!!!");
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            logtext.Text = "";
+        }
+
+        private void savelog_Click(object sender, EventArgs e)
+        {
+             SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+             saveFileDialog1.Filter = "Log fájl|*.log|txt fájl|*.txt";
+            saveFileDialog1.Title = "Mentés";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                string dirName =
+                   System.IO.Path.GetDirectoryName(saveFileDialog1.FileName);
+                string filename =
+                 System.IO.Path.GetFileName(saveFileDialog1.FileName);
+                StreamWriter sw = File.CreateText(saveFileDialog1.FileName);
+                for (int i = 0; i < logtext.Lines.Length; i++)
+                {
+                    sw.WriteLine(logtext.Lines[i]);
+                }
+                sw.Flush();
+                sw.Close();
+            }
+        }
+
+        private void checkinterval_ValueChanged(object sender, EventArgs e)
+        {
+            checktimer.Interval = Convert.ToInt32(checkinterval.Value) * 1000;
+        }
+    }
+}
